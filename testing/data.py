772b38c070aad21026af514d17f7f100d8b3ac2f
SeredaYehor <sirjeleshka@gmail.conf>
2022-11-18 10:42:16 +0200

    Added scripts for Ci/CD testing

---
+++ testing/data.py
@@ -0,0 +1,5 @@
+#Data for testing space station
+#Change input array with moving directions and output with expected coords or map props
+
+input = ["W", "A", "S", "D", "A", "A", "A", "A"]
+output = ["20 19", "19 19", "19 20", "20 20", "19 20", "18 20", "17 20", "w"]
