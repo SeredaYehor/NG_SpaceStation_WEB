6e58da8917b08def068910d868ec9d66e0d7211e
SeredaYehor <sirjeleshka@gmail.conf>
2022-12-17 21:52:40 +0200

    Remove res var

--- testing/start_test.sh
+++ testing/start_test.sh
@@ -4,11 +4,10 @@ res=0
 python mapLoaderTest.py -v
 if [ $? -eq 1 ];
 then
-	$res = 1
+	exit 1
 fi
 python positionTest.py -v
 if [ $? -eq 1 ];
 then
-        $res = 1
+        exit 1
 fi
-exit $res
