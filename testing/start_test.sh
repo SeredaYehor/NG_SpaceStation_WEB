7fed2e3298125ba442d3d3a8e032c520d9142604
SeredaYehor <sirjeleshka@gmail.conf>
2022-12-17 22:29:59 +0200

    Updated coverage report

--- testing/start_test.sh
+++ testing/start_test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 python -m coverage run mapLoaderTest.py -v
+python -m coverage report -m
 if [ $? -eq 1 ];
 then
 	exit 1
 fi
+
 python -m coverage run positionTest.py -v
+python -m coverage report -m
 if [ $? -eq 1 ];
 then
         exit 1
 fi
-python -m coverage report -m
