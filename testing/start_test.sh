91a57d1f4c63d0f745e10e96cce343c052782a59
SeredaYehor <sirjeleshka@gmail.conf>
2022-12-20 20:28:47 +0200

    Added printing code coverage as html

--- testing/start_test.sh
+++ testing/start_test.sh
@@ -2,6 +2,7 @@
 
 python -m coverage run mapLoaderTest.py -v
 python -m coverage report -m
+python -m coverage html
 if [ $? -eq 1 ];
 then
 	exit 1
@@ -9,6 +10,7 @@ fi
 
 python -m coverage run positionTest.py -v
 python -m coverage report -m
+python -m coverage html
 if [ $? -eq 1 ];
 then
         exit 1
